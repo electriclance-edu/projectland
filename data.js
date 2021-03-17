f9a8823c624d4b8b56a134c0f4076da21915722a
electriclance <lance_libatique@hotmail.com>
2021-03-18 05:33:53 +0800

    basic file creation
    also put some test project data

---
+++ data.js
@@ -0,0 +1,293 @@
+var projects = [
+  {
+    name:"Cakelabs 3",
+    startDate:"//~2015",
+    endDate:"//~2015",
+    description:"A silly little 'game' where you eat weird pastries.",
+    completeness:"60",
+    tags:"website,game",
+    effort:"1",
+    creators:"lance"
+  },
+  {
+    name:"Nihonese",
+    startDate:"12/10/2020",
+    endDate:"7/2/2021",
+    description:"A silly japanese learning tool.",
+    completeness:"100",
+    tags:"language,website,game",
+    effort:"1",
+    creators:"lance"
+  },
+  {
+    name:"Dead People Arriving Places",
+    startDate:"23/2/2020",
+    endDate:"ongoing",
+    description:"Three friends running a galactic pitstop in the middle of nowhere. People stop coming.",
+    completeness:"5",
+    tags:"art,writing",
+    effort:"1",
+    creators:"lance"
+  },
+  {
+    name:"EepOS",
+    startDate:"/~11/2020",
+    endDate:"/11/2020",
+    description:"The greatest javascript-based operating system, coming near you.",
+    completeness:"25",
+    tags:"website,game",
+    effort:"2",
+    creators:"lance"
+  },
+  {
+    name:"We Do It Better than Icarus Ever Could",
+    startDate:"start/~8/2020",
+    endDate:"middle/11/2020",
+    description:"Explore a dying galaxy. Currently just a spinny planet generator.",
+    completeness:"15",
+    tags:"website,game",
+    effort:"1",
+    creators:"lance"
+  },
+  {
+    name:"Fishing!",
+    startDate:"~25/~5/2020",
+    endDate:"~6/~6/2020",
+    description:"A typing fishing game. Based on a different mmorpg's fishing system.",
+    completeness:"50",
+    tags:"website,game",
+    effort:"2",
+    creators:"lance"
+  },
+  {
+    name:"'Waste of Time' the particle simulator",
+    startDate:"26/10/2020",
+    endDate:"27/10/2020",
+    description:"A falling-sand simulator made in a day. Which is why it's crap.",
+    completeness:"60",
+    tags:"website,game",
+    effort:"3",
+    creators:"lance"
+  },
+  {
+    name:"Burger Stand",
+    startDate:"29/12/2020",
+    endDate:"5/1/2021",
+    description:"Make burgers.",
+    completeness:"30",
+    tags:"website,game",
+    effort:"2",
+    creators:"lance"
+  },
+  {
+    name:"Sterile Neutrinos: A Hidden Unknown",
+    startDate:"middle/~5/2020",
+    endDate:"15/6/2020",
+    description:"A really badly made science video submitted for a competition. Reached 50th percentile ;D",
+    completeness:"100",
+    tags:"video",
+    effort:"1",
+    creators:"lance"
+  },
+  {
+    name:"Experiment #32",
+    startDate:"~26/2/2021",
+    endDate:"ongoing",
+    description:"Some research logs and spirits chronicling a junior librarian's attempt at commnading plants to do things.",
+    completeness:"30",
+    tags:"story,text",
+    effort:"2",
+    creators:"lance"
+  },
+  {
+    name:"Hurezu Kalii (name subject to change)",
+    startDate:"~26/2/2021",
+    endDate:"ongoing",
+    description:"Some might call it a terrible conlang. I call it a glorified name generator. The ceremonial language of Kalimba.",
+    completeness:"20",
+    tags:"language,text",
+    effort:"2",
+    creators:"lance"
+  },
+  {
+    name:"Kalimba",
+    startDate:"/~8/2020",
+    endDate:"ongoing",
+    description:"The story of a quaint little village by a small, quiet sea. Used for an ongoing minecraft roleplay.",
+    completeness:"40",
+    tags:"story,text,roleplay",
+    effort:"1",
+    creators:"lance"
+  },
+  {
+    name:"The Ezelhan Documentation of Foreign Ships",
+    startDate:"/~7/2020",
+    endDate:"14/7/2020",
+    description:"Drew a few ship designs, made them in cardboard, wrote some lore for them.",
+    completeness:"100",
+    tags:"story,text,crafts",
+    effort:"3",
+    creators:"lance"
+  },
+  {
+    name:"colorless gold",
+    startDate:"5/10/2020",
+    endDate:"6/10/2020",
+    description:"An intern's story working at the Chromatics Division. There are colors beyond human comprehension.",
+    completeness:"5",
+    tags:"story,text",
+    effort:"3",
+    creators:"lance"
+  },
+  {
+    name:"The Wovenstrong Dual",
+    startDate:"~2/12/2020",
+    endDate:"middle/12/2020",
+    description:"A grand nation spanning two galaxies born from the union of two wholly different cultures. Unrelated to Kalimba.",
+    completeness:"60",
+    tags:"story,text,roleplay",
+    effort:"3",
+    creators:"lance"
+  },
+  {
+    name:"Higurashi no Naku Koro Ni Poster",
+    startDate:"10/6/2020",
+    endDate:"10/6/2020",
+    description:"A beautiful pixel-art poster commemorating a great anime.",
+    completeness:"100",
+    tags:"art",
+    effort:"3",
+    creators:"lance"
+  },
+  {
+    name:"ASS - UGH UGH",
+    startDate:"start/3/2020",
+    endDate:"~4/9/2020",
+    description:"(pronounced ASS - ɻçʝxɣχʁħʕhɦ) The Universal Government of Hornies Uterus Go Hard. Beauty incarnate.",
+    completeness:"100",
+    tags:"story,text,roleplay,censor",
+    effort:"2",
+    creators:"lance,lauren,justice"
+  },
+  {
+    name:"Constructures",
+    startDate:"mid/12/2019",
+    endDate:"~12/3/2020",
+    description:"A supplementary for any verbal roleplay, designed to keep track of custom resources, characters, etc.",
+    completeness:"10",
+    tags:"roleplay,website,game",
+    effort:"2",
+    creators:"lance"
+  },
+  {
+    name:"One Mile",
+    startDate:"middle/4/2020",
+    endDate:"/~6/2020",
+    description:"Massive creatures walk across an infinite plane, carrying the remnants away from an ever expanding darkness.",
+    completeness:"5",
+    tags:"story,text",
+    effort:"2",
+    creators:"lance"
+  },
+  {
+    name:"One Mile + 1",
+    startDate:"16/2/2021",
+    endDate:"hiatus",
+    description:"A rebooted version of One Mile because the concept was nice.",
+    completeness:"5",
+    tags:"story,text",
+    effort:"3",
+    creators:"lance"
+  },
+  {
+    name:"Aea",
+    startDate:"/~2/2020",
+    endDate:"/3/2020",
+    description:"The evolutionary tree of a world called Aea. Lots of lines. Also has some lore.",
+    completeness:"100",
+    tags:"art",
+    effort:"1",
+    creators:"lance"
+  },
+  {
+    name:"Cookies and Crickets",
+    startDate:"2/2/2021",
+    endDate:"24/2/2021",
+    description:"A man finds himself alone in a seemingly endless, alien desert.",
+    completeness:"15",
+    tags:"art,story,text",
+    effort:"2",
+    creators:"lance"
+  },
+  {
+    name:"Stories and Snippets/Soul",
+    startDate:"14/10/2020",
+    endDate:"/1/2021",
+    description:"Many, many attempts at extending Sam's 'soul' concept.",
+    completeness:"30",
+    tags:"story,text",
+    effort:"2",
+    creators:"lance,sam"
+  },
+  {
+    name:"Hexagonland",
+    startDate:"/~2/2021",
+    endDate:"/2/2021",
+    description:"A recently-awakened god tries to bring back life to their ruined, deserted world.",
+    completeness:"10",
+    tags:"story,game,website",
+    effort:"3",
+    creators:"lance"
+  },
+  {
+    name:"Starbound",
+    startDate:"day/month/2020",
+    endDate:"day/month/2020",
+    description:"A man reminisces beside his friends' graves.",
+    completeness:"100",
+    tags:"art,story",
+    effort:"2",
+    creators:"lance"
+  },
+  {
+    name:"734 Words",
+    startDate:"27/5/2020",
+    endDate:"28/5/2020",
+    description:"A tragic tale written in a poetic style reminiscent of the great epics of old.",
+    completeness:"100",
+    tags:"text,story",
+    effort:"3",
+    creators:"lance"
+  },
+  {
+    name:"Projectland",
+    startDate:"18/3/2021",
+    endDate:"ongoing",
+    description:"A listing of me and my friends' projects.",
+    completeness:"10",
+    tags:"website",
+    effort:"2",
+    creators:"lance,whoever submitted projects"
+  },
+  {
+    name:"Da Estorya",
+    startDate:"8/3/2021",
+    endDate:"ongoing",
+    description:"A little writing experiment where one person writes a page, then another writes the next, and so on, cycling till it is done.",
+    completeness:"10",
+    tags:"story,text",
+    effort:"2",
+    creators:"lance,liam"
+  },
+  {
+    name:"default",
+    startDate:"day/month/year",
+    endDate:"day/month/year",
+    description:"default",
+    completeness:"default",
+    tags:"default,default",
+    effort:"default",
+    creators:"default"
+  },
+]
+//date display should be very casual, like the date /~11/2020 would be interpreted as "sometime around november last year"
